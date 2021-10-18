482362e80c4c2ec919d5c717bb3316153554a0b0
dengyakui <dengyakui@outlook.com>
2021-10-18 20:34:51 +0800

    init

---
+++ TestProject1/CalculatorTests.cs
@@ -0,0 +1,28 @@
+﻿using CoveCoverDemo;
+using Xunit;
+
+namespace TestProject1
+{
+    public class CalculatorTests
+    {
+        private readonly Calculator _calculator = new();
+
+        [Fact]
+        public void Test_Add()
+        {
+            var one = 1;
+            var two = 2;
+            var actual = _calculator.Add(one, two);
+            Assert.Equal(3, actual);
+        }
+
+        [Fact]
+        public void Test_Subtract()
+        {
+            var one = 1;
+            var two = 2;
+            var actual = _calculator.Subtract(one, two);
+            Assert.Equal(-1, actual);
+        }
+    }
+}
