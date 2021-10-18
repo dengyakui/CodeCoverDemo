b5d2b39b040bde4600260d9f25933cc69bbd5bb0
dengyakui <dengyakui@outlook.com>
2021-10-18 21:24:57 +0800

    add more logic

--- CoveCoverDemo/Calculator.cs
+++ CoveCoverDemo/Calculator.cs
@@ -4,8 +4,14 @@
     {
         public int Add(int one, int two)
         {
-            if (one == 0) return two;
-            return one + two;
+            var transform = one switch
+            {
+                0 => 0,
+                1 => -1,
+                2 => -2,
+                _ => -3
+            };
+            return transform + two;
         }
 
         public int Subtract(int one, int two)
